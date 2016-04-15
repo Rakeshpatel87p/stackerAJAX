bdaae91460838984b13037d5f650df322e9eafe0
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-15 19:30:47 +0530

    almost done

--- js/app.js
+++ js/app.js
@@ -1,94 +1,165 @@
 // this function takes the question object returned by the StackOverflow request
 // and returns new result to be appended to DOM
 var showQuestion = function(question) {
-	
-	// clone our result template code
-	var result = $('.templates .question').clone();
-	
-	// Set the question properties in result
-	var questionElem = result.find('.question-text a');
-	questionElem.attr('href', question.link);
-	questionElem.text(question.title);
 
-	// set the date asked property in result
-	var asked = result.find('.asked-date');
-	var date = new Date(1000*question.creation_date);
-	asked.text(date.toString());
+    // clone our result template code
+    var result = $('.templates .question').clone();
 
-	// set the .viewed for question property in result
-	var viewed = result.find('.viewed');
-	viewed.text(question.view_count);
+    // Set the question properties in result
+    var questionElem = result.find('.question-text a');
+    questionElem.attr('href', question.link);
+    questionElem.text(question.title);
 
-	// set some properties related to asker
-	var asker = result.find('.asker');
-	asker.html('<p>Name: <a target="_blank" '+
-		'href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
-		question.owner.display_name +
-		'</a></p>' +
-		'<p>Reputation: ' + question.owner.reputation + '</p>'
-	);
+    // set the date asked property in result
+    var asked = result.find('.asked-date');
+    var date = new Date(1000 * question.creation_date);
+    asked.text(date.toString());
 
-	return result;
+    // set the .viewed for question property in result
+    var viewed = result.find('.viewed');
+    viewed.text(question.view_count);
+
+    // set some properties related to asker
+    var asker = result.find('.asker');
+    asker.html('<p>Name: <a target="_blank" ' +
+        'href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+        question.owner.display_name +
+        '</a></p>' +
+        '<p>Reputation: ' + question.owner.reputation + '</p>'
+    );
+
+    return result;
 };
 
 
 // this function takes the results object from StackOverflow
 // and returns the number of results and tags to be appended to DOM
 var showSearchResults = function(query, resultNum) {
-	var results = resultNum + ' results for <strong>' + query + '</strong>';
-	return results;
+    var results = resultNum + ' results for <strong>' + query + '</strong>';
+    return results;
 };
 
+var showSearchResultsInspiration = function(item) {
+    // console.log(item);
+    var resultInspiration = $('.templates .inspiration').clone();
+
+    // name
+    var nameElem = resultInspiration.find('.name');
+    nameElem.text(item.user.display_name);
+    // profileLink
+    var profileLinkElem = resultInspiration.find('.profile-link');
+    profileLinkElem.attr('href', item.user.link);
+    profileLinkElem.text('Profile Link');
+
+    // score
+    var scoreElem = resultInspiration.find('.score p');
+    scoreElem.text(item.score);
+        
+    // acceptanceRate
+    var acceptRateElem = resultInspiration.find('.accept-rate p');
+    acceptRateElem.text(item.user.accept_rate);
+    	// if (acceptRateElem == false) {
+    	// 	acceptRateElem.text('Not provided');
+    	// }
+    // postCount
+    var postCountElem = resultInspiration.find('.post-count p');
+    postCountElem.text(item.post_count);
+    console.log(item.post_count);
+
+    return resultInspiration;
+
+};
+
+
 // takes error string and turns it into displayable DOM element
-var showError = function(error){
-	var errorElem = $('.templates .error').clone();
-	var errorText = '<p>' + error + '</p>';
-	errorElem.append(errorText);
+var showError = function(error) {
+	console.log(error);
+    var errorElem = $('.templates .error').clone();
+    var errorText = '<p>' + error + '</p>';
+    errorElem.append(errorText);
 };
 
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
 var getUnanswered = function(tags) {
-	
-	// the parameters we need to pass in our request to StackOverflow's API
-	var request = { 
-		tagged: tags,
-		site: 'stackoverflow',
-		order: 'desc',
-		sort: 'creation'
-	};
-	
-	$.ajax({
-		url: "http://api.stackexchange.com/2.2/questions/unanswered",
-		data: request,
-		dataType: "jsonp",//use jsonp to avoid cross origin issues
-		type: "GET",
-	})
-	.done(function(result){ //this waits for the ajax to return with a succesful promise object
-		var searchResults = showSearchResults(request.tagged, result.items.length);
 
-		$('.search-results').html(searchResults);
-		//$.each is a higher order function. It takes an array and a function as an argument.
-		//The function is executed once for each item in the array.
-		$.each(result.items, function(i, item) {
-			var question = showQuestion(item);
-			$('.results').append(question);
-		});
-	})
-	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
-		var errorElem = showError(error);
-		$('.search-results').append(errorElem);
-	});
+    // the parameters we need to pass in our request to StackOverflow's API
+    var request = {
+        tagged: tags,
+        site: 'stackoverflow',
+        order: 'desc',
+        sort: 'creation'
+    };
+
+    $.ajax({
+            url: "http://api.stackexchange.com/2.2/questions/unanswered",
+            data: request,
+            dataType: "jsonp", //use jsonp to avoid cross origin issues
+            type: "GET",
+
+        })
+        .done(function(result) { //this waits for the ajax to return with a succesful promise object
+            var searchResults = showSearchResults(request.tagged, result.items.length);
+
+            $('.search-results').html(searchResults);
+            //$.each is a higher order function. It takes an array and a function as an argument.
+            //The function is executed once for each item in the array.
+            $.each(result.items, function(i, item) {
+                var question = showQuestion(item);
+                $('.results').append(question);
+            });
+        })
+        // error not properly functioning
+        .fail(function(jqXHR, error) { //this waits for the ajax to return with an error promise object
+            var errorElem = showError(error);
+            console.log(errorElem);
+            $('.search-results').append(errorElem);
+        });
 };
 
+var getAnswerers = function(tagsInspiration) {
+    var params = {
+        site: 'stackoverflow',
+    }
 
-$(document).ready( function() {
-	$('.unanswered-getter').submit( function(e){
-		e.preventDefault();
-		// zero out results if previous search has run
-		$('.results').html('');
-		// get the value of the tags the user submitted
-		var tags = $(this).find("input[name='tags']").val();
-		getUnanswered(tags);
-	});
+    $.ajax({
+            url: 'http://api.stackexchange.com/2.2/tags/' + tagsInspiration + '/top-answerers/all_time',
+            data: params,
+            dataType: "jsonp", //use jsonp to avoid cross origin issues
+            type: "GET",
+
+        })
+        .done(function(result) { //this waits for the ajax to return with a succesful promise object
+            $.each(result.items, function(index, item) {
+                var showInspiration = showSearchResultsInspiration(item);
+                $('.results').append(showInspiration);
+
+            });
+
+        })
+        .fail(function(jqXHR, error) { //this waits for the ajax to return with an error promise object
+            console.log("yep");
+            var errorElem = showError(error);
+            $('.search-results').append(errorElem);
+        });
+
+}
+
+$(document).ready(function() {
+    $('.unanswered-getter').submit(function(e) {
+        e.preventDefault();
+        // zero out results if previous search has run
+        $('.results').html('');
+        // get the value of the tags the user submitted
+        var tags = $(this).find("input[name='tags']").val();
+        getUnanswered(tags);
+    });
+
+    $('.inspiration-getter').submit(function(e) {
+        e.preventDefault();
+        $('.results').html('');
+        var tagsInspiration = $(this).find("input[name='answerers']").val();
+        getAnswerers(tagsInspiration);
+
+    });
 });
